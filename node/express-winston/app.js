a7e432268569ee04ced56daedf9ea36afa3fb6d4
Zach Sherman <zksherm@gmail.com>
2018-05-08 10:23:09 -0400

    change from config to utils in winston example

--- node/express-winston/app.js
+++ node/express-winston/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
-var winston = require('./config/winston');
+var log = require('./utils/log');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(morgan('tiny', { stream: winston.stream }));
+app.use(morgan('tiny', { stream: log.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
